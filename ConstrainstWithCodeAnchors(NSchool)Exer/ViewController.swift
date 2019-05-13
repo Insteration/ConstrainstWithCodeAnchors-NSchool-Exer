9be78d3fb6664b5aaa55b47f9127e7a780223f68
Art Karma <inste@icloud.com>
2019-05-13 20:49:09 +0300

    Initial Commit

---
+++ ConstrainstWithCodeAnchors(NSchool)Exer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConstrainstWithCodeAnchors(NSchool)Exer
+//
+//  Created by Art Karma on 5/13/19.
+//  Copyright © 2019 Art Karma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
